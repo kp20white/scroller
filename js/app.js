3aab4411c15cf7162bd3de9bda115b935f5922e0
Kyle White <kwhite@franklinamerican.com>
2014-05-06 17:12:56 -0500

    got rotating arms...w00t

--- js/app.js
+++ js/app.js
@@ -1,17 +1,43 @@
 /**
  * Created by kwhite on 5/5/2014.
  */
+var scroller,
+    paperWidth,
+    paperHeight,
+    scrollerPaddingLeft,
+    scrollerPaddingRight,
+    paperWidthOffset,
+    paperHeightOffset
+;
+
+var playerHeadRadius = 20,
+    playerBodyLength = 30,
+    playerArmLength = 20,
+    playerArmSeparationAngle = 45,
+    playerLegLength = 25,
+    playerStartX,
+    playerArmStartY,
+    playerArmJoint
+;
+
 function createScrollerWindow() {
-    var scroller = $('.scroller-canvas');
-    var paperWidth = parseInt(scroller.css('width'));
-    var paperHeight = parseInt(scroller.css('height'));
-    var scrollerPaddingLeft = parseInt(scroller.css('padding-left'));
-    var scrollerPaddingRight = parseInt(scroller.css('padding-right'));
-    var paperOffset = scroller.offset();
+    scroller = $('.scroller-canvas'),
+    paperWidth = parseInt(scroller.css('width')),
+    paperHeight = parseInt(scroller.css('height')),
+    scrollerPaddingLeft = parseInt(scroller.css('padding-left')),
+    scrollerPaddingRight = parseInt(scroller.css('padding-right')),
+    paperHeightOffset = parseInt(scroller.offset().top),
+    paperWidthOffset = parseInt(scroller.offset().left)
+    ;
+
     var adjustedPaperWidth = paperWidth - (scrollerPaddingLeft + scrollerPaddingRight);
     var paper = Raphael(scroller.get(0), adjustedPaperWidth, paperHeight);
     var drawingGrid = paper.rect(0, 0, adjustedPaperWidth, paperHeight, 5);
     drawingGrid.attr({fill: "#fff", stroke: "#000"});
+
+    var player = createPlayer(paper, drawingGrid, adjustedPaperWidth, paperHeight);
+
+    drawingGrid.mousemove(grab);
 
     var scrollerBaseline = paperHeight - 50;
     // First, draw the baseline
@@ -20,17 +46,17 @@ function createScrollerWindow() {
     var enemies = [];
 
     var defaultEnemySquareWidth = "30";
-    for(var x = 1; x <= 10; x++) {
-        (function(x) {
-            window.setTimeout(function () {
-                if (x % 2 == 0) {
-                    createEnemySquare(paper, {"width": defaultEnemySquareWidth, "verticalBaseline": scrollerBaseline, "horizontalBoundary": { "right": adjustedPaperWidth, "left": (-2 * defaultEnemySquareWidth)}});
-                } else {
-                    createEnemyCircle(paper, {"radius": "15", "verticalBaseline": scrollerBaseline, "horizontalBoundary": { "right": adjustedPaperWidth, "left": (-2 * defaultEnemySquareWidth)}});
-                }
-            }, 3000 * x);
-        })(x);
-    }
+//    for(var x = 1; x <= 10; x++) {
+//        (function(x) {
+//            window.setTimeout(function () {
+//                if (x % 2 == 0) {
+//                    createEnemySquare(paper, {"width": defaultEnemySquareWidth, "verticalBaseline": scrollerBaseline, "horizontalBoundary": { "right": adjustedPaperWidth, "left": (-2 * defaultEnemySquareWidth)}});
+//                } else {
+//                    createEnemyCircle(paper, {"radius": "15", "verticalBaseline": scrollerBaseline, "horizontalBoundary": { "right": adjustedPaperWidth, "left": (-2 * defaultEnemySquareWidth)}});
+//                }
+//            }, 3000 * x);
+//        })(x);
+//    }
 
 }
 
@@ -47,4 +73,64 @@ function createEnemyCircle(paper, enemyDimensions) {
     var enemyCircle = paper.circle(enemyDimensions.horizontalBoundary.right+enemyCircleDiameter, enemyDimensions.verticalBaseline - (enemyCircleRadius), enemyCircleRadius)
     enemyCircle.attr({"stroke-width": "3"});
     enemyCircle.animate({cx: enemyDimensions.horizontalBoundary.left}, 5000);
+}
+
+/**
+ * Draw the player
+ * @param Raphael.Element paper
+ * @param array-like object containing the player Raphael Elements
+ */
+function createPlayer(paper, playerGrid, paperWidth, paperHeight) {
+    playerStartX = (paperWidth / 2),
+    playerArmStartY = ((paperHeight / 2) + playerHeadRadius + (playerBodyLength / 2)),
+    playerArmJoint = "M" + playerStartX + " " + playerArmStartY;
+    var player = paper.set();
+    var playerHead = paper.circle(playerStartX, paperHeight/2, playerHeadRadius),
+        playerBody = paper.path("M"+playerStartX+" "+((paperHeight/2)+playerHeadRadius)+"L"+playerStartX+" "+((paperHeight/2)+playerHeadRadius+playerBodyLength)),
+        playerLeftArm = paper.path(playerArmJoint+"L"+(playerStartX+playerArmLength)+" "+playerArmStartY),
+        playerRightArm = paper.path(playerArmJoint+"L"+(playerStartX-playerArmLength)+" "+playerArmStartY),
+        playerLeftLeg = paper.path("M"+playerStartX+" "+((paperHeight/2)+playerHeadRadius+(playerBodyLength))+"L"+(playerStartX+playerLegLength)+" "+((paperHeight/2)+playerHeadRadius+playerBodyLength+playerLegLength)),
+        playerRightLeg = paper.path("M"+playerStartX+" "+((paperHeight/2)+playerHeadRadius+(playerBodyLength))+"L"+(playerStartX-playerLegLength)+" "+((paperHeight/2)+playerHeadRadius+playerBodyLength+playerLegLength))
+        ;
+    playerHead.attr({"stroke-width": "2"});
+    playerBody.attr({"stroke-width": "3"});
+    playerLeftArm.attr({"stroke-width": "2"});
+    playerRightArm.attr({"stroke-width": "2"});
+    (function(centerX, centerY) {
+        playerGrid.mousemove(function(evt) {
+            var grabTargetX = (evt.clientX - paperWidthOffset) - centerX,
+                grabTargetY = (paperHeightOffset + evt.clientY) - centerY,
+                grabTargetAngle = Math.atan2(grabTargetY, grabTargetX);
+            ;
+//            console.log("eventX: " + evt.clientX + " eventY: " + evt.clientY + " tx: " + paperWidthOffset + " ty: " + paperHeightOffset + " centerX: " + centerX + " centerY: " + centerY);
+            playerLeftArm.transform("r"+radians2degrees(grabTargetAngle)+","+centerX+","+centerY);
+            playerRightArm.transform("r"+(radians2degrees(grabTargetAngle - Math.PI) - playerArmSeparationAngle)+","+centerX+","+centerY);
+        });
+    })(playerStartX, playerArmStartY);
+
+    playerLeftLeg.attr({"stroke-width": "2"});
+    playerRightLeg.attr({"stroke-width": "2"});
+    player.push(
+        playerHead,
+        playerBody,
+        playerLeftArm,
+        playerRightArm,
+        playerLeftLeg,
+        playerRightLeg
+    );
+    return player;
+}
+
+/**
+ * handle mouse event for player 'grab'
+ * @param evt
+ */
+function grab(evt) {
+}
+
+function kick(evt) {
+}
+
+function radians2degrees(radians) {
+    return radians * 180 / Math.PI;
 }
